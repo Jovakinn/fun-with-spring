06cd4e21997654b6412258d1b3359c688ea858c1
Jovakinn <max05012004@gmail.com>
2021-11-07 22:04:31 +0200

    add person construction

--- src/main/java/spring_introduction/TestIoC.java
+++ src/main/java/spring_introduction/TestIoC.java
@@ -3,13 +3,17 @@ package spring_introduction;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring_introduction.model.Person;
 
+import java.util.logging.Logger;
+
 public class TestIoC {
+    private static final Logger log = Logger.getLogger(TestIoC.class.getName());
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+       log.info(person.toString());
 
         context.close();
     }
--- src/main/java/spring_introduction/model/Person.java
+++ src/main/java/spring_introduction/model/Person.java
@@ -1,15 +1,76 @@
 package spring_introduction.model;
 
+import spring_introduction.model.Companies.Companies;
+
 import java.util.logging.Logger;
 
 public class Person {
     private static final Logger log = Logger.getLogger(Person.class.getName());
 
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private Companies companies;
     private Pet pet;
 
     public Person(Pet pet) {
         log.info("Person bean is created");
         this.pet = pet;
+    }
+
+    public Person() {
+    }
+
+    public void setPet(Pet pet) {
+        log.info("Class Person: set pet");
+        this.pet = pet;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", companies=" + companies +
+                ", pet=" + pet +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Companies getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(Companies companies) {
+        this.companies = companies;
+    }
+
+    public Pet getPet() {
+        return pet;
     }
 
     public void callYourPet() {
