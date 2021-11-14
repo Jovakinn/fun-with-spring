55740f2e864ed2d1109beb78b7b3e612f23f418f
Jovakinn <max05012004@gmail.com>
2021-11-14 15:54:16 +0200

    add JoinPool to advice

--- src/main/java/aop/aspects/ExceptionHandlingAspect.java
+++ src/main/java/aop/aspects/ExceptionHandlingAspect.java
@@ -1,9 +1,13 @@
 package aop.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 @Component
@@ -13,7 +17,15 @@ public class ExceptionHandlingAspect {
     private static final Logger log = Logger.getLogger(ExceptionHandlingAspect.class.getName());
 
     @Before("aop.aspects.MyPointcuts.allGetMethods()")
-    public void beforeGetExceptionHandlingAdvice() {
+    public void beforeGetExceptionHandlingAdvice(JoinPoint joinPoint) {
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Object[] arguments = joinPoint.getArgs();
+
+        log.info("Method signature: " + methodSignature);
+        log.info("Method signature.getMethod().getName = " +
+                methodSignature.getMethod().getName());
+        log.info("Arguments: " + Arrays.toString(arguments));
         log.info("Catch and handle exceptions");
     }
 }
--- src/main/java/aop/aspects/MyPointcuts.java
+++ src/main/java/aop/aspects/MyPointcuts.java
@@ -3,6 +3,6 @@ package aop.aspects;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class MyPointcuts {
-    @Pointcut("execution(public void get*())")
+    @Pointcut("execution(public void getBooks())")
     public void allGetMethods() {}
 }
