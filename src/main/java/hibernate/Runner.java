7b8af70d070e99b33d37aba6206d82163b0e3958
Jovakinn <max05012004@gmail.com>
2021-11-18 22:22:14 +0200

    add runner

--- src/main/java/hibernate/Runner.java
+++ src/main/java/hibernate/Runner.java
@@ -2,11 +2,15 @@ package hibernate;
 
 import hibernate.DAO.Service.EmployeeService;
 import hibernate.model.Employee;
+import java.util.logging.Logger;
 
 public class Runner {
+    private static final Logger log = Logger.getLogger(Runner.class.getName());
+
     public static void main(String[] args) {
         EmployeeService employeeService = new EmployeeService();
         Employee employee = new Employee("Max", "Khodakov", "IT", 1200);
         employeeService.saveEmployee(employee);
+        log.info(String.valueOf(employeeService.findAllEmployees()));
     }
 }
